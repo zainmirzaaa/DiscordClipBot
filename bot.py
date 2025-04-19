ed89b788084f29398206170ab0ac70732f3327c0
zainmirzaaa <zainnmirzaa@gmail.com>
2025-04-18 19:14:00 -0600

    bot skeleton

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot is online as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+if __name__ == "__main__":
+    bot.run("YOUR_DISCORD_BOT_TOKEN")
