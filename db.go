a964fa3372ac0d0e2ec7c4103eb432400fbc081d
zainmirzaaa <zainnmirzaa@gmail.com>
2025-04-19 22:41:00 -0600

    mongo setup

---
+++ db.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+func connectMongo(uri string) *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected to MongoDB")
+	return client
+}
