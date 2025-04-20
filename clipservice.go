7f06a8c3c88ca16429408d31ce8f07b3d5a43a24
zainmirzaaa <zainnmirzaa@gmail.com>
2025-04-19 19:33:00 -0600

    fetch clips

---
+++ clipservice.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Clip struct {
+	ID   string `json:"id"`
+	Link string `json:"link"`
+}
+
+func clipsHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: fetch from Azure Blob, mocked for now
+	results := []Clip{
+		{ID: "1", Link: "https://azure.blob/clip1.mp4"},
+		{ID: "2", Link: "https://azure.blob/clip2.mp4"},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+}
+
+func main() {
+	http.HandleFunc("/clips", clipsHandler)
+	log.Println("clip service running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
